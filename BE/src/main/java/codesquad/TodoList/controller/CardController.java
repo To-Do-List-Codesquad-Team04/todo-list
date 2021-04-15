fe87ddd967f5eba8f255bf003bad348e640dcba5
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-04-15 17:55:58 +0900

    ResponseEntity 를 이용해, 응답 메세지 발송하도록 메서드 수정

--- BE/src/main/java/codesquad/TodoList/controller/CardController.java
+++ BE/src/main/java/codesquad/TodoList/controller/CardController.java
@@ -4,6 +4,8 @@ import codesquad.TodoList.Dto.CardDto;
 import codesquad.TodoList.Dto.CardList;
 import codesquad.TodoList.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,8 +20,9 @@ public class CardController {
 
     // CREATE
     @PostMapping("/cards")
-    public void newCard(@RequestBody CardDto cardDto) {
+    public ResponseEntity newCard(@RequestBody CardDto cardDto) {
         cardService.create(cardDto);
+        return new ResponseEntity<>("SUCCESS", HttpStatus.CREATED);
     }
 
     // READ
@@ -30,13 +33,15 @@ public class CardController {
 
     // UPDATE
     @PutMapping("/cards/{id}")
-    public void editCards(@PathVariable Long id, @RequestBody CardDto newCardDto) {
+    public ResponseEntity editCards(@PathVariable Long id, @RequestBody CardDto newCardDto) {
         cardService.edit(id, newCardDto);
+        return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
     }
 
     // DELETE
     @DeleteMapping("/cards/{id}")
-    public void deleteCards(@PathVariable Long id) {
+    public ResponseEntity deleteCards(@PathVariable Long id) {
         cardService.delete(id);
+        return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
     }
 }
